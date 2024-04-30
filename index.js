16ac92589a85c88430cb82965b545ae4e91ac964
Syed Saud Ali <160472981+SyedSaudAli@users.noreply.github.com>
2024-04-30 18:03:16 +0500

    uptodate

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ let answers = await inquirer.prompt([
     {
         name: "courses",
         type: "list",
-        message: "Select your course enrolled",
+        message: "Select your course enrolled: ",
         choices: [
             "TypeScript",
             "JavaScript",
@@ -30,6 +30,7 @@ let answers = await inquirer.prompt([
         ],
     },
 ]);
+const studBalance = 0;
 const courseFees = {
     TypeScript: 6000,
     JavaScript: 5000,
@@ -39,18 +40,19 @@ const courseFees = {
     CSS: 2000,
     Ms_Office: 1000,
 };
-console.log(chalk.bgBlue(`Course Fees: ${courseFees[answers.courses]}/-`));
+console.log(chalk.bgBlue(`\n\tCourse Fees: ${courseFees[answers.courses]}/-`));
+console.log(chalk.blue(`\tBalance: ${studBalance}\n`));
 let payment_method = await inquirer.prompt([
     {
         name: "payment",
         type: "list",
-        message: "Enter your Payment Mathod",
+        message: "Enter your Payment Mathod.",
         choices: ["Bank Transfer", "Easy Paisa", "Jazz Cash"],
     },
     {
         name: "amount",
         type: "input",
-        message: "Transfer Money",
+        message: "Transfer Money:",
         validate: function (value) {
             if (value.trim() !== "") {
                 return true;
@@ -59,15 +61,31 @@ let payment_method = await inquirer.prompt([
         },
     },
 ]);
-console.log(`You select Payment Method ${payment_method.payment}\n`);
 const courseFee = courseFees[answers.courses];
 const paymentAmount = parseFloat(payment_method.amount);
 if (courseFee === paymentAmount) {
     console.log(chalk.bold.bgCyan.whiteBright("\tCongratulations, you have purchased this course.\n"));
-    console.log(chalk.green(`Student Name: ${chalk.bold.underline(answers.students)}`));
-    console.log(chalk.green(`Student ID: ${chalk.bold.underline(randomNumber)}`));
-    console.log(chalk.green(`Course: ${chalk.bold.underline(answers.courses)}`));
+    let viewAndExit = await inquirer.prompt([
+        {
+            name: "information",
+            type: "list",
+            message: "Student info",
+            choices: ["View", "Exit"],
+        },
+    ]);
+    if (viewAndExit.information === "View") {
+        console.log(chalk.bold.bgCyan.whiteBright(`\tCongratulations, ${answers.students} you have purchased this course.\n`));
+        console.log(chalk.bold.blue(" Your Status..."));
+        console.log(chalk.green(`Student Name: ${chalk.bold.underline(answers.students)}`));
+        console.log(chalk.green(`Student ID: ${chalk.bold.underline(randomNumber)}`));
+        console.log(chalk.green(`Course: ${chalk.bold.underline(answers.courses)}`));
+        console.log(chalk.green(`Course Fees: ${chalk.bold.underline(paymentAmount)}`));
+        console.log(chalk.green(`Balance: ${chalk.bold.underline(studBalance + paymentAmount)}`));
+    }
+    else {
+        console.log(chalk.redBright("\n\tExisting student Management system\n"));
+    }
 }
 else {
-    console.log(chalk.redBright("invalid amount due to course\n"));
+    console.log(chalk.redBright("\n\tinvalid amount due to course\n"));
 }
--- index.ts
+++ index.ts
@@ -25,7 +25,7 @@ let answers = await inquirer.prompt([
   {
     name: "courses",
     type: "list",
-    message: "Select your course enrolled",
+    message: "Select your course enrolled: ",
     choices: [
       "TypeScript",
       "JavaScript",
@@ -37,6 +37,9 @@ let answers = await inquirer.prompt([
     ],
   },
 ]);
+
+const studBalance: number = 0;
+
 const courseFees: { [key: string]: number } = {
   TypeScript: 6000,
   JavaScript: 5000,
@@ -47,19 +50,20 @@ const courseFees: { [key: string]: number } = {
   Ms_Office: 1000,
 };
 
-console.log(chalk.bgBlue(`Course Fees: ${courseFees[answers.courses]}/-`));
+console.log(chalk.bgBlue(`\n\tCourse Fees: ${courseFees[answers.courses]}/-`));
+console.log(chalk.blue(`\tBalance: ${studBalance}\n`));
 
 let payment_method = await inquirer.prompt([
   {
     name: "payment",
     type: "list",
-    message: "Enter your Payment Mathod",
+    message: "Enter your Payment Mathod.",
     choices: ["Bank Transfer", "Easy Paisa", "Jazz Cash"],
   },
   {
     name: "amount",
     type: "input",
-    message: "Transfer Money",
+    message: "Transfer Money:",
     validate: function (value) {
       if (value.trim() !== "") {
         return true;
@@ -68,7 +72,6 @@ let payment_method = await inquirer.prompt([
     },
   },
 ]);
-console.log(`You select Payment Method ${payment_method.payment}\n`);
 
 const courseFee = courseFees[answers.courses];
 const paymentAmount = parseFloat(payment_method.amount);
@@ -79,11 +82,42 @@ if (courseFee === paymentAmount) {
       "\tCongratulations, you have purchased this course.\n"
     )
   );
-  console.log(
-    chalk.green(`Student Name: ${chalk.bold.underline(answers.students)}`)
-  );
-  console.log(chalk.green(`Student ID: ${chalk.bold.underline(randomNumber)}`));
-  console.log(chalk.green(`Course: ${chalk.bold.underline(answers.courses)}`));
+
+  let viewAndExit = await inquirer.prompt([
+    {
+      name: "information",
+      type: "list",
+      message: "Student info",
+      choices: ["View", "Exit"],
+    },
+  ]);
+  if (viewAndExit.information === "View") {
+    console.log(
+      chalk.bold.bgCyan.whiteBright(
+        `\tCongratulations, ${answers.students} you have purchased this course.\n`
+      )
+    );
+    console.log(chalk.bold.blue(" Your Status..."));
+    console.log(
+      chalk.green(`Student Name: ${chalk.bold.underline(answers.students)}`)
+    );
+    console.log(
+      chalk.green(`Student ID: ${chalk.bold.underline(randomNumber)}`)
+    );
+    console.log(
+      chalk.green(`Course: ${chalk.bold.underline(answers.courses)}`)
+    );
+    console.log(
+      chalk.green(`Course Fees: ${chalk.bold.underline(paymentAmount)}`)
+    );
+    console.log(
+      chalk.green(
+        `Balance: ${chalk.bold.underline(studBalance + paymentAmount)}`
+      )
+    );
+  } else {
+    console.log(chalk.redBright("\n\tExisting student Management system\n"));
+  }
 } else {
-  console.log(chalk.redBright("invalid amount due to course\n"));
+  console.log(chalk.redBright("\n\tinvalid amount due to course\n"));
 }
